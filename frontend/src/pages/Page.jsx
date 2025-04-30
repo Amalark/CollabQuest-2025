82f57f61402fe88f11622e41a721c6e192bbe81f
alinazir105 <alinazir105@gmail.com>
2025-04-30 11:48:13 +0500

    project section pushed

--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Project from "@/components/Projects/Project";
 const Page = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-purple-800">RETARD</h1>
+      <Project/>
     </div>
   );
 };
